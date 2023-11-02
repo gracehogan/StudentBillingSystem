3e893c2ff2c28f86921ea08f571006b318f173f6
shonanohs <shonafitzpatrick15@gmail.com>
2023-11-02 09:58:50 +0000

    Update Graduate.java

--- src/main/java/org/example/Graduate.java
+++ src/main/java/org/example/Graduate.java
@@ -1,4 +1,12 @@
 package org.example;
 
-public class Graduate {
+public class Graduate extends Student {
+    private static final double DISCOUNT_RATE = 0.3;
+
+    @Override
+    protected double applyDiscount(double feePreDiscount) {
+        double currentFee = feePreDiscount - (feePreDiscount * DISCOUNT_RATE);
+
+        return currentFee;
+    }
 }
